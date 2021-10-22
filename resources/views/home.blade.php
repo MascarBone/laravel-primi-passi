5a0838556c98e7e3e7535bcfcb7f44eb388e44f3
MascarBone <simone.bonelli@gmail.com>
2021-10-22 16:07:16 +0200

    Inizialized

---
+++ resources/views/home.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        ul {
+            list-style: none;
+        }
+        li {
+            margin: 0 10px;
+        }
+        .mt-5 {
+            margin-top: 50px;
+        }
+        .flex-center {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .text-center {
+            text-align: center;
+        }
+    </style>
+    <title>My Home</title>
+</head>
+<body>
+    <header class="text-center">
+        
+        <h1 class="mt-5">This is my first Laravel page</h1>
+        
+        <ul class="mt-5 flex-center">
+            <li>
+                <a href="">First Page</a>
+            </li>
+            <li>
+                <a href="">Second Page</a>
+            </li>
+            <li>
+                <a href="">Third Page</a>
+            </li>
+            <li>
+                <a href="">Fourth Page</a>
+            </li>
+        </ul>
+    </header>
+</body>
+</html>
